b57110957299547ea4d2606c29664470d0bc34ff
Jhon Calingco <calingco.je@gmail.com>
2022-06-25 20:34:18 +0800

    first section of landing page

--- components/nav.js
+++ components/nav.js
@@ -1,18 +1,48 @@
 const navBarTemplate = document.createElement('template');
 navBarTemplate.innerHTML = `
 
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <link href="css/navBar.css" rel="stylesheet"></link>
 
-<div class="nav-container">
-  <img src="#">
-    <ul class="navBar">
-      <li><a href="./html.html">Html</a></li>
-      <li><a href="#">Css</a></li>
-      <li><a href="#">JavaScript</a></li>
-      <li><a href="#">Frameworks</a></li>
-    </ul>
-    <input type="text" id="search" placeholder="Search...">
-</div>
+
+<!-- HEADER / NAVBAR -->
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <div class="container-fluid nav">
+                <!-- LOGO -->
+                <a class="navbar-brand" href="#"><img src="#"></a>
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Html</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">CSS</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">JavaScript</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                                Frameworks
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <li><a class="dropdown-item" href="#">Bootstrap</a></li>
+                                <li><a class="dropdown-item disabled" href="#">Node.js</a></li>
+                                <li><a class="dropdown-item disabled" href="#">Laravel</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
 `
 
 
